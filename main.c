dec18b65b6a8c185ffbc38d33f14cf3490bd3f50
alexhutman <alexhutman@gmail.com>
2017-06-28 15:10:41 -0400

    Cleaned up code
    Looks nicer and made x0, y0 constants.
    I didn't want to have the user enter the step size or numIterations because they are dependent on one another. There is more granularity if they enter those variable themselves

--- main.c
+++ main.c
@@ -3,17 +3,14 @@
 #include <math.h>
 
 #define PI 3.14159265
+#define x0 0.0
+#define y0 0.0
 
-double step = PI/400.0;
-int numIterations = 100;
+double step = PI/400.0;  //Calculate these based on the (x,y) you are trying to calculate,
+int numIterations = 100; //for this program will calculate f(x0 + step*numIterations)
 
-double x0 = 0.0;    //-|
-                    // |-set both of these at the same time
-double curX = 0.0;  //-|
-
-double y0 = 0.0;    //-|
-                    // |-set both of these at the same time
-double curY = 0.0;  //-|
+double curX = x0;
+double curY = y0;
 
 double tempK1 = 0.0;
 double tempK2 = 0.0;
@@ -22,37 +19,37 @@ double tempK3 = 0.0;
 int main()
 {
     for(int i=0; i<numIterations; i++) {
-        rungeKutta(curX,curY);
+        rungeKutta(curX, curY);
     }
 
     printf("f(%f) = %f", curX, curY);
     return 0;
 }
 
-double k1(double x_,double y_) {  //PUT MAIN EQUATION HERE
-    tempK1 = cos(x_)+0*y_;
+double k1(double x_, double y_) {
+    tempK1 = cos(x_);       //PUT f'(x,y) HERE!
     return tempK1;
 }
 
-double k2(double x_,double y_) {
+double k2(double x_, double y_) {
     tempK2 = k1(x_ + (step/2.0), y_ + (tempK1*step/2.0));
     return tempK2;
 }
 
-double k3(double x_,double y_) {
+double k3(double x_, double y_) {
     tempK3 = k1(x_ + (step/2.0), y_ + (tempK2*step/2.0));
     return tempK3;
 }
 
-double k4(double x_,double y_) {
+double k4(double x_, double y_) {
     return k1(x_ + step, y_ + step*tempK3);
 }
 
-double t4(double x_,double y_) {
+double t4(double x_, double y_) {
     return (1.0/6.0)*(k1(x_, y_) + 2.0*k2(x_, y_) + 2.0*k3(x_, y_) + k4(x_, y_));
 }
 
 void rungeKutta(double x_, double y_) {
-    curY = curY + step*t4(curX,curY);
+    curY = curY + step*t4(curX, curY);
     curX += step;
 }
